6429bb59f164c2d534b3f9a203779fdc25baf68b
SomeDiesYoung <vladimer.osmanoviDOIT@geolab.edu.ge>
2024-10-22 17:18:41 +0400

    Adding Task_2

---
+++ Task_2/Task_2.cs
@@ -0,0 +1,52 @@
+﻿namespace Task_2;
+
+internal class Task_2
+{
+    static void Main()
+    {
+        Person person = new Person();
+        person.Describe();
+
+        Student student = new Student();
+        student.Describe();
+
+        Teacher teacher = new Teacher();
+        teacher.Describe();
+
+
+        Console.ReadKey();
+    }
+
+    public class Person
+    {
+
+        public virtual void Describe()
+        {
+            Console.WriteLine("I am a Person");
+        }
+
+
+    }
+
+    public class Student : Person
+    {
+
+        public override void Describe()
+        {
+            Console.WriteLine("I am a Student");
+        }
+
+
+    }
+    public class Teacher : Person
+    {
+
+        public override void Describe()
+        {
+            Console.WriteLine("I am a Teacher");
+        }
+
+
+    }
+}
+
